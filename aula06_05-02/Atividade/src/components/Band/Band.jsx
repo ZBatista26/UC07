bdd57d5f432c835f8fd4ccc50678eb9adf46a157
FRANCISCO BATISTA DA SILVA NETO <a95405294@rn.senac.local>
2025-03-12 17:17:21 -0300

    fix: correção justfy

--- aula06_05-02/Atividade/src/components/Band/Band.jsx
+++ aula06_05-02/Atividade/src/components/Band/Band.jsx
@@ -4,15 +4,13 @@ import Style from "./Band.module.css"
 function Band({imagem, nome, descricao}) {
     return (
         
-        <section className={Style.Band}>
-        <div className="Band">
+        <div className={Style.Band}>
             <img src={imagem} alt={nome}></img>
             <div className="Bandinfo">
                 <h2>{nome}</h2>
                 <p>{descricao}</p>
             </div>
         </div>
-        </section>
     
 )
 

643ce597e63b0d1c7050a453a3bd0be69b0c78f8
Batista <franciscobatista744@gmail.com>
2025-03-12 11:19:43 -0300

    add: parte da atividade adicionando react a aula 06, seguda parte

--- aula06_05-02/Atividade/src/App.jsx
+++ aula06_05-02/Atividade/src/App.jsx
@@ -1,12 +1,16 @@
 import React from"react";
 import Header from"./components/Header/Header"
 import Footer from"./components/Footer/Footer"
+import Aside from"./components/Aside/Aside"
+import Band from"./components/Band/Band"
+
 
 function App() {
  return(
   <>
      <Header/>
-
+    <Band/>
+    <Aside/>
     <Footer/>
   </>
  )
---
+++ aula06_05-02/Atividade/src/components/Main/Main.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Style from "./Main.module.css";
+
+function Main(){
+    return (
+        <main className={Style.Main}>
+              <section>
+            <div className="Band">
+                <img src="https://cdn.ontourmedia.io/gunsnroses/site_v2/animations/gnr_loop_logo_01.jpg" alt="Banda 1"/>
+                <div className="Band-info">
+                    <h2>Banda 1</h2>
+                    <p>Descrição da Banda 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div className="Band">
+                <img src="https://m.media-amazon.com/images/I/91WaJU5cCXL._AC_UF1000,1000_QL80_.jpg" alt="Banda 2"/>
+                <div className="Band-info">
+                    <h2>Banda 2</h2>
+                    <p>Descrição da Banda 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className="Band">
+                <img src="https://m.media-amazon.com/images/I/91WaJU5cCXL._AC_UF1000,1000_QL80_.jpg" alt="Banda 2"/>
+                <div className="Band-info">
+                    <h2>Banda 3</h2>
+                    <p>Descrição da Banda 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className="Band">
+                <img src="https://m.media-amazon.com/images/I/91WaJU5cCXL._AC_UF1000,1000_QL80_.jpg" alt="Banda 2"/>
+                <div className="Band-info">
+                    <h2>Banda 4</h2>
+                    <p>Descrição da Banda 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+        </section>
+
+        </main>
+    )
+}
+
+export default Main

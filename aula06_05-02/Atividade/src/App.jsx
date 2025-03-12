300a94172314a53d461eebf6812e67b6eb9d0dc6
FRANCISCO BATISTA DA SILVA NETO <a95405294@rn.senac.local>
2025-03-12 13:51:57 -0300

    add: Atividade, banda de rock, parte final

--- aula06_05-02/Atividade/src/App.jsx
+++ aula06_05-02/Atividade/src/App.jsx
@@ -3,13 +3,14 @@ import Header from"./components/Header/Header"
 import Footer from"./components/Footer/Footer"
 import Aside from"./components/Aside/Aside"
 import Band from"./components/Band/Band"
+import Main from "./components/Main/Main";
 
 
 function App() {
  return(
   <>
      <Header/>
-    <Band/>
+    <Main/>
     <Aside/>
     <Footer/>
   </>
--- aula06_05-02/Atividade/src/components/Main/Main.jsx
+++ aula06_05-02/Atividade/src/components/Main/Main.jsx
@@ -5,9 +5,9 @@ function Main(){
     return (
         <main className={Style.Main}>
               <section>
-            <div className="Band">
+            <div className={Style.Band}>
                 <img src="https://cdn.ontourmedia.io/gunsnroses/site_v2/animations/gnr_loop_logo_01.jpg" alt="Banda 1"/>
-                <div className="Band-info">
+                <div className={Style.Bandinfo}>
                     <h2>Banda 1</h2>
                     <p>Descrição da Banda 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
@@ -15,27 +15,27 @@ function Main(){
         </section>
 
         <section>
-            <div className="Band">
+            <div className={Style.Band}>
                 <img src="https://m.media-amazon.com/images/I/91WaJU5cCXL._AC_UF1000,1000_QL80_.jpg" alt="Banda 2"/>
-                <div className="Band-info">
+                <div className={Style.Bandinfo}>
                     <h2>Banda 2</h2>
                     <p>Descrição da Banda 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
             </div>
         </section>
         <section>
-            <div className="Band">
+            <div className={Style.Band}>
                 <img src="https://m.media-amazon.com/images/I/91WaJU5cCXL._AC_UF1000,1000_QL80_.jpg" alt="Banda 2"/>
-                <div className="Band-info">
+                <div className={Style.Bandinfo}>
                     <h2>Banda 3</h2>
                     <p>Descrição da Banda 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
             </div>
         </section>
         <section>
-            <div className="Band">
+            <div className={Style.Band}>
                 <img src="https://m.media-amazon.com/images/I/91WaJU5cCXL._AC_UF1000,1000_QL80_.jpg" alt="Banda 2"/>
-                <div className="Band-info">
+                <div className={Style.Bandinfo}>
                     <h2>Banda 4</h2>
                     <p>Descrição da Banda 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
